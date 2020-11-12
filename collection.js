e77c54a7618f6f3d51b55dfb0d1969a67569129a
cemil <cemilyilmaz.be@gmail.com>
2020-11-12 14:17:38 +0100

    commit branch

---
+++ collection.js
@@ -0,0 +1,94 @@
+const collection = [
+    {
+        title: '',
+        author: '',
+        category: ['', ''],
+        link: '',
+        description: ''
+    },
+    {
+        title: 'Avengers Endgame',
+        author: 'Joe Russo, Anthony Russo',
+        category: ['Action'],
+        link: 'https://www.youtube.com/watch?v=wV-Q0o2OQjQ&ab_channel=MarvelFR',
+        description: 'Thanos ayant anéanti la moitié de l’univers, les Avengers restants resserrent les rangs dans ce vingt-deuxième film des Studios Marvel.'
+    },
+    {
+        title: 'Pacific Rim',
+        author: 'Guillermo del Toro, Steven S.',
+        category: ['Action'],
+        link: 'https://www.youtube.com/watch?v=qOX_O118N7k&ab_channel=WarnerBros.France',
+        description: 'Des hordes de créatures monstrueuses venues d’ailleurs, ont déclenché une guerre. Pour les combattres, de gigantesques robots on été fabriquer.'
+    },
+    {
+        title: 'Charlie la chocolaterie',
+        author: 'Tim Burton',
+        category: ['Comédie'],
+        link: 'https://www.youtube.com/watch?v=xjZihSC42HI&ab_channel=TimBurtonFR',
+        description: 'Willy Wonka ouvre les portes de sa fabirque de chocolat à cinq enfants qui découvrent les secrets de ses extraordinaires créations gustatives.'
+    },
+    {
+        title: 'Moi moche et mechant',
+        author: 'Pierre Coffin, Chris Renaud',
+        category: ['Animation'],
+        link: 'https://www.youtube.com/watch?v=OPf1YYSKxBs&ab_channel=UniversalPicturesFrance',
+        description: 'Gru, un méchant vilain, entouré de sous-fifres et armé jusqu’aux dents, qui complote le plus gros casse de tous les temps : voler la lune (Oui, la lune !)...'
+    },
+    {
+        title: 'Dragon',
+        author: 'Dean DeBlois, Chris Sanders',
+        category: ['Animation', 'Action'],
+        link: 'https://www.youtube.com/watch?v=rsj6Iv2jE1I&ab_channel=3DmaniaTV',
+        description: 'Harold, jeune Viking, dans sa tribu combattre les dragons est le sport national. Sa vie va être bouleversée par sa rencontre avec un dragon.'
+    },
+    {
+        title: 'Ted',
+        author: 'Seth MacFarlane',
+        category: ['Comédie'],
+        link: 'https://www.youtube.com/watch?v=esfilNg464I&ab_channel=FilmsActu',
+        description: 'Le souhait de John Bennett donne vie à son ami ours en peluche Ted. À l âge adulte, Ted empêche John et son amour Lori Collins de poursuivre leur vie.'
+    },
+    {
+        title: 'World War Z',
+        author: 'Marc Forster',
+        category: ['Horreur', 'Action'],
+        link: 'https://www.youtube.com/watch?v=jTrmqThqpK8&ab_channel=FilmsActu',
+        description: 'Un agent des Nations Unies se lance dans une course contre la montre pour mettre fin à la prolifération d un virus mortel menaçant l espèce humaine.'
+    },
+    {
+        title: 'Conjuring',
+        author: 'James Wan',
+        category: ['Horreur'],
+        link: 'https://www.youtube.com/watch?v=Mkf9rGLXeuE&ab_channel=LesCin%C3%A9masPath%C3%A9Gaumont',
+        description: 'Cette histoire abominable, mais vraie, est celle d une famille qui aurait préféré ne pas rencontrer les esprits qui hantent sa ferme isolée.'
+    },
+    {
+        title: 'San Andreas',
+        author: 'Brad Peyton',
+        category: ['Action'],
+        link: 'https://www.youtube.com/watch?v=HGpwaIf3ZKk&ab_channel=WarnerBros.France',
+        description: 'Lorsque la Faille de San Andreas s ouvrir, un pilote d hélicoptère et la femme quittent Los Angeles pour San Francisco dans l espoir de sauver leur fille unique.'
+    },
+    {
+        title: 'Kitikou et la sorcière',
+        author: 'Michel Ocelot',
+        category: ['Animation'],
+        link: 'https://www.youtube.com/watch?v=3EA4xnPpZhk&ab_channel=SortieDvdBluRay',
+        description: 'Kirikou naît dans un village d’Afrique sur lequel la sorcière Karaba a jeté un terrible sort. Kirikou veut délivrer le village de l’emprise maléfique de Karaba.'
+    },
+]
+
+
+document.querySelector('.supp').innerHTML=''
+const mainContainer
+
+for (let elment of collection) {
+    
+
+    
+}
+const newDiv = document.createElement('div');
+
+const title = document
+title.textContent= element.title;
+
